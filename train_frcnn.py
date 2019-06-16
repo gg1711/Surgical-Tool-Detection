4d02dc24fd8ca54476235c8da0316d47068285fa
Gaurav Gupta <35700631+gg1711@users.noreply.github.com>
2019-06-17 03:55:31 +0530

    Update train_frcnn.py

--- train_frcnn.py
+++ train_frcnn.py
@@ -146,7 +146,8 @@ model_all = Model([img_input, roi_input], rpn[:2] + classifier)
 
 try:
 	print('loading weights from {}'.format(C.base_net_weights))
-	model_rpn.load_weights(C.base_net_weights, by_name=True)
+	model_rpn.load_weights(C.base_net_weights, by_name=True) 
+	#By keeping by name=True it only loads weights upto common layers in both models
 	model_classifier.load_weights(C.base_net_weights, by_name=True)
 except:
 	print('Could not load pretrained model weights. Weights can be found in the keras application folder \
